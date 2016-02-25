a140eab7b40f6fbe646cf9f4c801bb835fd3e182
ekforduk <ekforduk@gmail.com>
2016-02-25 19:43:09 +0000

    initial files

---
+++ RunPiFit.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import os, sys
+from random import randint
+
+LedPin = 11    # pin11 --- led
+BtnPin = 12    # pin12 --- button
+
+Led_status = 1
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.setup(BtnPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
+	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+
+def swLed(ev=None):
+	global Led_status
+	Led_status = not Led_status
+	GPIO.output(LedPin, Led_status)  # switch led status(on-->off; off-->on)
+	if Led_status == 1:
+		print 'led off...'
+	else:
+		print '...led on'
+		r = randint(1,5)
+		print r
+		if r==1:
+			os.system('omxplayer -o local /home/pi/PIFit/5Lunges.m4a')
+		elif r==2:
+			os.system('omxplayer -o local /home/pi/PIFit/4Starjumps.m4a')
+		elif r==3:
+			os.system('omxplayer -o local /home/pi/PIFit/1Burpies.m4a')
+		elif r==4:
+			os.system('omxplayer -o local /home/pi/PIFit/8Squats.m4a')
+		elif r==5:
+			os.system('omxplayer -o local /home/pi/PIFit/TouchTheGroundR.m4a')
+
+
+def loop():
+	GPIO.add_event_detect(BtnPin, GPIO.FALLING, callback=swLed) # wait for falling
+	while True:
+		pass   # Don't do anything
+
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH)     # led off
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
